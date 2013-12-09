f5dce6e159e2dbd1d5d5cd61156990f6574d3275
goblin <goblinprog@gmail.com>
2013-12-09 01:13:39 +0000

    Affichage console en tableau esthetique un peu

--- Controllers/Console.php
+++ Controllers/Console.php
@@ -7,13 +7,26 @@ class Console extends Controller {
 
 	public function execute(){
 		$model = new Model();
-		if($model->connected) echo "Connecte";
-		else die("Error");
 		$ret = $model->execute($_POST['query']);
 
-		foreach($ret as $r){
-			print_r($r);
+		$attributs = get_object_vars($ret[0]);
+		
+		echo '<table><tr>';
+		foreach($attributs as $k=>$v){
+			echo '<th>'.$k.'</th>';
 		}
+		echo '</tr>';
+
+		foreach($ret as $r){
+			echo '<tr>';
+			foreach($attributs as $k=>$v){
+				echo '<td>'.$r->$k.'</td>';
+			}
+			echo '</tr>';
+		}
+
+		echo '</table>';
+
 	}
 
 }
